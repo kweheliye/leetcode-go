bd1a335a9c06187ffc223753ed70cc08ca18e5c9
Kamal Hashi <kamalweheliye@gmail.com>
2025-04-02 13:27:34 +0400

    bruteforce solution

--- arrays/2873_easy_maximum_triplet_value.go
+++ arrays/2873_easy_maximum_triplet_value.go
@@ -1,5 +1,16 @@
 package arrays
 
-func maximumTripletValue(nums []int) int64 {
-	panic("Not implemented")
+func MaximumTripletValueBruteForce(nums []int) int64 {
+	var n int = len(nums)
+	var res int64 = 0
+
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			for k := j + 1; k < n; k++ {
+				res = max(res, int64(nums[i]-nums[j])*int64(nums[k]))
+			}
+		}
+	}
+
+	return res
 }
