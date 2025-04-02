9aa8cd7923e65610986501e5e9413ac4dffc388e
Kamal Hashi <kamalweheliye@gmail.com>
2025-04-02 18:30:22 +0400

    Greedy approach

--- arrays/2873_easy_maximum_triplet_value.go
+++ arrays/2873_easy_maximum_triplet_value.go
@@ -14,3 +14,20 @@ func MaximumTripletValueBruteForce(nums []int) int64 {
 
 	return res
 }
+
+func MaximumTripletValueGreedy(nums []int) int64 {
+	var n int = len(nums)
+	var res int64 = 0
+	left := nums[0]
+
+	for j := 1; j < n; j++ {
+		left = max(left, nums[j])
+
+		for k := j + 1; k < n; k++ {
+			res = max(res, int64(left-nums[j])*int64(nums[k]))
+		}
+
+	}
+
+	return res
+}
--- main.go
+++ main.go
@@ -1,52 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-var shortGolang = "Watch Go crash course"
-var fullGolang = "Watch Nana's Golang Full Course"
-var rewardDessert = "Reward myself with a donut"
-var taskItems = []string{shortGolang, fullGolang, rewardDessert}
-
 func main() {
-	//https://www.youtube.com/watch?v=2oXBOaVowYY
-	// a := []int{1, 2, 3}
-	// b := append(a[:1], 10)
-
-	// fmt.Println(b)
-	// fmt.Println(a)
-	// message := fmt.Sprintf("Hello %v", a)
-	// fmt.Println(message)
-
-	fmt.Printf("##### Welcome to our TodoList App! ######")
-
-	http.HandleFunc("/", showTasks)
-
-	http.ListenAndServe(":8080", nil)
-
-}
-
-func showTasks(writer http.ResponseWriter, reader *http.Request) {
-	for _, task := range taskItems {
-		fmt.Fprintln(writer, task)
-	}
-}
-
-func helloUser(writer http.ResponseWriter, requet *http.Request) {
-	var greeting = "Hello user. Welcome to our TodoList App!"
-	fmt.Fprint(writer, greeting)
-
-}
-
-func printTasks(taskItems []string) {
-	fmt.Println("List of my Todos")
-	for index, task := range taskItems {
-		fmt.Printf("%d. %s\n", index+1, task)
-	}
-}
-
-func addTask(taskItems []string, newTask string) []string {
-	return append(taskItems, newTask)
+	panic("Not implemented yet!")
 }
