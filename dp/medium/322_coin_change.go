d87e2a3562c1baf71fab81fe7f26a8f3bbd953d5
Kamal Hashi <kamalweheliye@gmail.com>
2025-05-02 15:56:07 +0400

    322 with memoization top-down

--- dp/medium/322_coin_change.go
+++ dp/medium/322_coin_change.go
@@ -4,33 +4,42 @@ import (
 	"math"
 )
 
-// IN PROGRESS ADD MEMO ARRAY
 func coinChange(coins []int, amount int) int {
-	return helper(coins, amount)
-}
+	memo := make([]int, amount+1)
 
-func helper(coins []int, remain int) int {
-	if remain == 0 {
-		return 0
-	}
-	if remain < 0 {
-		return -1
+	for i := range memo {
+		memo[i] = -1
 	}
 
-	minCount := math.MaxInt32
-
-	for _, coin := range coins {
-		count := helper(coins, remain-coin)
-		if count == -1 {
-			continue
+	// helper is a recursive function with memoization
+	var helper func(int) int
+	helper = func(remain int) int {
+		if remain == 0 {
+			return 0
 		}
-		minCount = min(minCount, count+1)
+		if remain < 0 {
+			return -1 // invalid case
+		}
+		if memo[remain] != -1 {
+			return memo[remain] // return already computed value
+		}
+
+		var minCount = math.MaxInt32
+		for _, coin := range coins {
+			count := helper(remain - coin) // try using this coin
+			if count == -1 {
+				continue // skip invalid subproblem
+			}
+			minCount = min(minCount, count+1)
+		}
+
+		// Save the result in memo
+		if minCount == math.MaxInt32 {
+			return -1
+		}
+		memo[remain] = minCount
+		return memo[remain]
 	}
 
-	if minCount == math.MaxInt32 {
-		return -1
-	}
-
-	return minCount
-
+	return helper(amount)
 }
