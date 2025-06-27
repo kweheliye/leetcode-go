4fcc644428c9eab181830af32ced688d28a0ab21
Kamal Weheliye <kamalweheliye@gmail.com>
2025-06-27 09:46:58 +0400

    roman to integer solution

---
+++ arrays/easy/13_roman_to_integer.go
@@ -0,0 +1,28 @@
+package easy
+
+func romanToInt(s string) int {
+	values := map[rune]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	sum := 0
+	n := len(s)
+
+	for i := 0; i < n; i++ {
+		currValue := values[rune(i)]
+
+		if i+1 < n && currValue < values[rune(s[i+1])] {
+			sum += currValue - values[rune(s[i+1])]
+		} else {
+			sum += currValue
+		}
+
+	}
+	return sum
+}
---
+++ arrays/easy/13_roman_to_integer_test.go
@@ -0,0 +1,36 @@
+package easy
+
+import "testing"
+
+func TestRomanToInt(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Test Case 1",
+			args: args{
+				s: "III",
+			},
+			want: 3,
+		},
+		{
+			name: "Test Case 2",
+			args: args{
+				s: "IV",
+			},
+			want: 4,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := romanToInt(tt.args.s); got != tt.want {
+				t.Errorf("romanToInt() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
