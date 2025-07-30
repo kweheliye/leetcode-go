2ee06352be17a2c7b016861678ebc3b85c0eb91a
kamalweheliye <kamalweheliye@gmail.com>
2025-07-30 09:24:53 +0400

    121 solution

---
+++ arrays/easy/121_best_time_to_buy_and_sell_stock.go
@@ -0,0 +1,16 @@
+package easy
+
+import "math"
+
+func maxProfit(prices []int) int {
+	minPrice := math.MaxInt
+	maxProfit := 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if prices[i]-minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+	}
+	return maxProfit
+}
