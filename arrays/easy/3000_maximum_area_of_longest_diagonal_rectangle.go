ff0d1235dce714ef4bcd96f68c7bfac500fdff67
kamalweheliye <kamalweheliye@gmail.com>
2025-08-26 10:08:24 +0400

    3000 solution

---
+++ arrays/easy/3000_maximum_area_of_longest_diagonal_rectangle.go
@@ -0,0 +1,21 @@
+package easy
+
+func areaOfMaxDiagonal(dimensions [][]int) int {
+	maxDiaSq := 0
+	maxArea := 0
+	for _, dim := range dimensions {
+		l := dim[0]
+		w := dim[1]
+		diaSq := l*l + w*w
+		area := l * w
+		if diaSq > maxDiaSq {
+			maxDiaSq = diaSq
+			maxArea = area
+		} else if diaSq == maxDiaSq {
+			if area > maxArea {
+				maxArea = area
+			}
+		}
+	}
+	return maxArea
+}
